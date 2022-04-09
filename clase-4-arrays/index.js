968e28b5f94a47fef9465d94b18c60bcb3926819
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-04-09 10:37:55 -0300

    after arrays

--- clase-4-arrays/index.js
+++ clase-4-arrays/index.js
@@ -143,55 +143,51 @@ console.log(listaNombres);
 //map, some, filter
 
 /* let info = [
-  {nombre: "cafe", precio: 1000, disponible: true},
-  {nombre: "pan", precio: 800, disponible: true},
-  {nombre: "leche", precio: 2000, disponible: true}
-]
-let result = info.filter(x => x.nombre == "cafe");
-let result1 = info.filter(x => x.precio > 900);
+  { nombre: "cafe", precio: 1000, disponible: true },
+  { nombre: "pan", precio: 800, disponible: false },
+  { nombre: "leche", precio: 2000, disponible: true },
+]; */
+
+/* let result = info.filter((x) => x.nombre == "cafe");
+let result1 = info.filter((x) => x.precio > 900);
+let result3 = info.map((x) => x.nombre);
+
 console.log(result);
 console.log(result1);
-console.log(info.map(x => x.nombre));
-console.log(info.some(x => x.precio > 3000));
-let productosDisponibles = info.filter(x => x.disponible === true);
-console.log(productosDisponibles); */
+console.log(result3); */
 
-/* class Producto {
+/* console.log(info.map((x) => x.nombre));
+console.log(info.some((x) => x.precio > 3000)); */
+
+/* let productosDisponibles = info.filter((x) => x.disponible === true);
+console.log(productosDisponibles);
+ */
+
+class Producto {
   constructor(id, nombre, precio) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
   }
 }
-let productoVirtuales = [
-  new Producto(1, "computador", 1000010),
-  new Producto(1, "computador1", 1001000),
-  new Producto(1, "computador2", 1020000),
+
+/* let productoVirtuales = [
+  new Producto(1, "computador", 100000),
+  new Producto(2, "computador1", 1039309),
+  new Producto(3, "computador2", 482791827),
 ];
-console.log(productoVirtuales[0].nombre); */
 
-/* class Producto {
-  constructor(id, nombre, precio) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-  }
-}
+console.log(productoVirtuales[0].nombre); */
 
 let productoVirtuales = [];
 
 for (let i = 0; i < 2; i++) {
-  let id = prompt("ingrese el id del producto");
-  let nombre = prompt("ingrese el nombre del producto");
-  let precio = prompt("ingrese el precio del producto");
+  let id = prompt("Ingrese el id del producto");
+  let nombre = prompt("Ingrese el nombre del producto");
+  let precio = prompt("Ingrese el precio del producto");
 
   productoVirtuales.push(new Producto(id, nombre, precio));
 }
-
-[
-  { nombre: "1234", id: 5 },
-  { nombre: "1231234", id: 6 },
-];
 
 function eliminarProducto(id) {
   let index = productoVirtuales.indexOf(
@@ -201,4 +197,4 @@ function eliminarProducto(id) {
   productoVirtuales.splice(index, 1);
 }
 
-eliminarProducto(6); */
+eliminarProducto(6);
